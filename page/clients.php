516576648b28f4cb7a3cf710903fbfb9994e6f88
sinharakesh <rksinha.btech@gmail.com>
2017-04-01 18:21:34 +0530

    client ledger added

--- page/clients.php
+++ page/clients.php
@@ -11,7 +11,23 @@ class page_clients extends \xepan\base\Page {
 		parent::init();
 
 		$c = $this->add('xepan\hr\CRUD');
-		$c->setModel('xavoc\securityservices\Client');
+		$model = $this->add('xavoc\securityservices\Model_Client');
+		$model->addHook('afterSave',function($m){
+			$debtor = $m->add('xepan\accounts\Model_Group')->load("Sundry Debtor");
+			$m->add('xepan\accounts\Model_Ledger')
+					->createNewLedger(
+									$m['name'],
+									$debtor->id,
+									$m->id,
+										[
+											'ledger_type'=>'Client',
+											'LedgerDisplayName'=>$m['name'],
+											'contact_id'=>$m->id
+										]
+									);
+		});
+
+		$c->setModel($model);
 
 	}
 }
