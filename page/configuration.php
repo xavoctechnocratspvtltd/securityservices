b989af81492cf893041e666e6d6cd13236104460
sinharakesh <rksinha.btech@gmail.com>
2017-07-20 16:50:33 +0530

    layout correction

--- page/configuration.php
+++ page/configuration.php
@@ -12,7 +12,15 @@ class page_configuration extends \xepan\base\Page {
 
 
 		$layout_model = $this->add('xavoc\securityservices\Model_Layout');
-		$crud = $this->add('xepan\hr\CRUD');
-		$crud->setModel($layout_model);
+		$crud = $this->add('xepan\hr\CRUD',['allow_edit'=>false]);
+		$crud->setModel($layout_model,['name']);
+
+		$crud->grid->addColumn('edit_layout');
+		$crud->grid->addHook('formatRow',function($g){
+			$g->current_row_html['edit_layout'] = '<a class="btn btn-info" href="?page=xavoc_secserv_layout&id='.$g->model['id'].'">Edit</a>';
+		});
+
+		$crud->grid->removeColumn('action');
+		$crud->grid->removeColumn('attachment_icon');
 	}
 }
---
+++ page/layout.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace xavoc\securityservices;
+
+
+class page_layout extends \xepan\base\Page {
+	
+	public $title ="Layout";
+
+	function init(){
+		parent::init();
+
+		$this->app->stickyGET('id');
+		if(!$_GET['id']){
+			throw new \Exception("id not found");
+		}
+
+		$layout = $this->add('xavoc\securityservices\Model_Layout');
+		$layout->load($_GET['id']);
+
+		$form = $this->add('Form');
+		$form->addField('line','name')->set($layout['name']);
+		$form->addField('xepan\base\RichText','master')->set($layout['master']);
+		$form->addField('xepan\base\RichText','detail')->set($layout['detail']);
+		$form->addSubmit('Save');
+
+		if($form->isSubmitted()){
+			$layout['master'] = $form['master'];
+			$layout['detail'] = $form['detail'];
+			$layout->save();
+
+			$form->js()->univ()->reload()->successMessage('Saved')->execute();
+		}
+
+	}
+}
