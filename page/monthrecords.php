fa8b24f06fb690106d8e680f4897bd72f2d8838d
sinharakesh <rksinha.btech@gmail.com>
2017-09-21 15:23:05 +0530

    month year record filter for month selection added

--- page/monthrecords.php
+++ page/monthrecords.php
@@ -10,8 +10,36 @@ class page_monthrecords extends \xepan\base\Page {
 	function init(){
 		parent::init();
 
+		$month_year = date('Y-m-01',strtotime($this->app->today));
+		if($this->app->stickyGET('month_year'))
+			$month_year = $_GET['month_year'];
+
+		$form = $this->add('Form');
+		$form->add('xepan\base\Controller_FLC')
+				->addContentSpot()
+				->makePanelsCoppalsible()
+				->layout([
+					'month_year'=>'Filter~c1~6~closed',
+					'FormButtons'=>'c2~6',
+				]);
+
+		$month_year_field = $form->addField('DatePicker','month_year')->validate('required')->set($month_year);
+		$month_year_field->options = [
+								'format'=> "yyyy-mm",
+			    				'startView'=> "months", 
+			    				'minViewMode'=> "months"
+			    			];
+		
+		$form->addSubmit('Filter')->addClass('btn btn-primary');
+
 		$c = $this->add('xepan\hr\CRUD');
+
+		if($form->isSubmitted()){
+			$form->js(null,$c->js()->reload(['month_year'=>$form['month_year']]))->execute();
+		}
+
 		$model = $this->add('xavoc\securityservices\Model_ClientMonthYear');
+		$model->addCondition('month_year',$month_year);
 		$model->setOrder('id','desc');
 
 		$c->setModel($model,['client_id','name','invoice_no','invoice_date','month_year','gross_amount','service_tax','service_tax_amount','net_amount'],['client','name','invoice_no','invoice_date','month_year','gross_amount','service_tax_amount','net_amount','status']);
